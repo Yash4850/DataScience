bbe94daa4719b235c81ec603af58e8918d7c5fe0
yash <yaswanthmaram4850@gmail.com>
2020-11-07 01:00:09 +0000

    Create NeuralNetHolder.py

---
+++ Rocket Landing Neural Netwotk/Data Collection/NeuralNetHolder.py
@@ -0,0 +1,10 @@
+
+class NeuralNetHolder:
+
+    def __init__(self):
+        super().__init__()
+
+    
+    def predict(self, input_row):
+        # WRITE CODE TO PROCESS INPUT ROW AND PREDICT THRUST, LEFT_TURNING AND RIGHT_TURNING
+        pass
